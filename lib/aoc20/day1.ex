8d9321e67614e72eae0e764df46841439779d06c
Will Strinz <will.strinz@drip.com>
2020-12-01 13:43:53 -0600

    day 1 part 2

--- lib/aoc20/day1.ex
+++ lib/aoc20/day1.ex
@@ -12,10 +12,38 @@ defmodule Aoc20.Day1 do
     |> Enum.find(&(&1 != nil))
   end
 
+  def check_triplets(first_number, second_number, numbers) do
+    case Enum.find(numbers, &(&1 + first_number + second_number == 2020)) do
+      nil -> nil
+      matched -> first_number * second_number * matched
+    end
+  end
+
+  def find_triplet_2020s(numbers) do
+    for(i <- numbers, j <- numbers, do: [i, j])
+    |> Enum.map(fn [first_number, second_number] ->
+      check_triplets(first_number, second_number, numbers)
+    end)
+    |> Enum.find(&(&1 != nil))
+  end
+
   def run() do
-    File.read!("inputs/day1.txt")
-    |> String.split("\n")
-    |> Enum.map(&(Integer.parse(&1) |> elem(0)))
-    |> find_2020s
+    numbers =
+      File.read!("inputs/day1.txt")
+      |> String.split("\n")
+      |> Enum.map(&(Integer.parse(&1) |> elem(0)))
+
+    part1 =
+      numbers
+      |> find_2020s
+
+    part2 =
+      numbers
+      |> find_triplet_2020s
+
+    IO.puts(~s"""
+    Part 1 answer: #{part1}
+    Part 2 answer: #{part2}
+    """)
   end
 end
