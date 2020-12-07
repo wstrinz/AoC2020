8c6e44484686a712a620002fbb7dcf58a36f1ab6
Will Strinz <will.strinz@drip.com>
2020-12-07 13:50:40 -0600

    day 7

--- lib/aoc20/day7.ex
+++ lib/aoc20/day7.ex
@@ -1,21 +1,65 @@
 defmodule Aoc20.Day7 do
-  def parse_rule(bag_rule) do
-    [[_, color]] = Regex.scan(~r/^(\w+ \w+) bags/, bag_rule)
+  def bag_count(start_color, rules) do
+    case Map.get(rules, start_color) do
+      [] ->
+        1
 
-    %{"color" => color, "children" => %{"color" => "adjective red", "count" => 2}}
+      children ->
+        1 +
+          (children
+           |> Enum.map(fn %{"color" => child_color, "count" => child_count} ->
+             child_count * bag_count(child_color, rules)
+           end)
+           |> Enum.sum())
+    end
+  end
+
+  def leads_to(start_color, rules, target_color) do
+    case Map.get(rules, start_color) do
+      [] ->
+        false
+
+      children ->
+        children
+        |> Enum.any?(fn %{"color" => child_color} ->
+          if child_color == target_color do
+            true
+          else
+            leads_to(child_color, rules, target_color)
+          end
+        end)
+    end
+  end
+
+  def parse_rule(bag_rule) do
+    [[_, color, contents_string]] = Regex.scan(~r/^(\w+ \w+) bags contain (.+)/, bag_rule)
+
+    parsed_children =
+      Regex.scan(~r/(\d+ \w+ \w+) bag/, contents_string)
+      |> Enum.map(fn [_, child_rule] ->
+        [[_, count, child_color]] = Regex.scan(~r/(\d+) (\w+ \w+)/, child_rule)
+        %{"color" => child_color, "count" => String.to_integer(count)}
+      end)
+
+    %{"color" => color, "children" => parsed_children}
   end
 
   def run() do
     bag_rules = File.read!("inputs/day7.txt") |> String.split("\n")
 
-    part1 =
+    parsed_rules =
       bag_rules
       |> Enum.map(&parse_rule/1)
       |> Enum.reduce(%{}, fn %{"color" => rule_color, "children" => children}, acc ->
         Map.put(acc, rule_color, children)
       end)
 
-    part2 = "??"
+    part1 =
+      Map.keys(parsed_rules)
+      |> Enum.map(&leads_to(&1, parsed_rules, "shiny gold"))
+      |> Enum.count(& &1)
+
+    part2 = bag_count("shiny gold", parsed_rules) - 1
 
     [part1, part2]
   end
